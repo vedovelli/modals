76a667b807579870af41ca0619e97c925b5f495c
Fábio Vedovelli <vedovelli@gmail.com>
2021-11-01 14:56:34 +0100

    Remove unnecessary content

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,9 @@
-/**
- * 1. Criar um componente único para ser o modal
- * 2. Criar um hook que receberá o componente a ser carregado como children
- * 3. O hook retornará um método para substituir children
- */
-
 import { ModalButton1, ModalButton2, Modal } from "../components";
 
 export default function Home() {
   return (
     <>
-      <Modal>
-        <p className="text-sm text-gray-500">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur
-          amet labore. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          Consequatur amet labore. Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Consequatur amet labore.
-        </p>
-      </Modal>
+      <Modal />
       <div className="container mx-auto mt-20">
         <div className="grid grid-cols-2 gap-4">
           <ModalButton1 />
